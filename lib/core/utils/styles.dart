48a7b1e26421841eee23af03ab9f393e5a6f0f97
Mahmoud Elsayed Eldeeb <https://github.com/MahmoudEldeeb10>
2026-02-01 20:41:28 +0200

    edit text styles

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -2,31 +2,23 @@ import 'package:flutter/cupertino.dart';
 import 'package:movies/constants.dart';
 
 class Styles {
-  static const textStyle30 = TextStyle(
-    fontSize: 30,
-    fontWeight: FontWeight.normal,
-    color: white,
-  );
-  static const textStyle20 = TextStyle(
-    fontSize: 20,
-    fontWeight: FontWeight.normal,
-    color: white,
-  );
-  static const styleText18 = TextStyle(
-    fontSize: 18,
-    fontWeight: FontWeight.w500,
-    color: white,
-  );
+  static textStyle30({Color color = white}) {
+    return TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: color);
+  }
 
-  static const styleText16 = TextStyle(
-    fontSize: 16,
-    fontWeight: FontWeight.w500,
-    color: white,
-  );
+  static textStyle20({Color color = white}) {
+    return TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: color);
+  }
 
-  static const styleText14 = TextStyle(
-    fontSize: 14,
-    fontWeight: FontWeight.w500,
-    color: white,
-  );
+  static TextStyle18({Color color = white}) {
+    return TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: color);
+  }
+
+  static textStyle16({Color color = white}) {
+    return TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: color);
+  }
+
+  static textStyle14({Color color = white}) {
+    return TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: color);
+  }
 }
--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:movies/core/utils/styles.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    return Scaffold(
+      body: Column(children: [Row(children: [
+            
+            ],
+          )]),
+    );
   }
 }
