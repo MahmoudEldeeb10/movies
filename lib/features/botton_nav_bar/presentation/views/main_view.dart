a246ea55b0470f3bd183b6a84075d88e22f7d545
Mahmoud Elsayed Eldeeb <https://github.com/MahmoudEldeeb10>
2026-02-01 18:56:05 +0200

    create btn nav bar & the main 3 views

--- lib/features/botton_nav_bar/presentation/views/main_view.dart
+++ lib/features/botton_nav_bar/presentation/views/main_view.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movies/features/botton_nav_bar/presentation/manager/cubit/bottom_nav_cubit.dart';
+import 'package:movies/features/home/presentation/views/home_view.dart';
+import 'package:movies/features/saved/persentation/views/saved_view.dart';
+import 'package:movies/features/search/presentation/views/search_view.dart';
 
 class MainView extends StatelessWidget {
   MainView({super.key});
-  final List pages = [
-    Center(child: Text('Home')),
-    Center(child: Text('Search')),
-    Center(child: Text('Saved')),
-  ];
+  final List pages = [HomeView(), SearchView(), SavedView()];
 
   @override
   Widget build(BuildContext context) {
